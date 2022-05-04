c14974c5315e49d18f157d6632d35133d4cdde53
Christian <ch.droguett.l@gmail.com>
2022-05-03 20:14:00 -0400

    se completa el formulario de registro y de logeo

--- views/Login.vue
+++ views/Login.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="login">
     <h1 class="title">Login in the page</h1>
-    <form action class="form">
+    <form action class="form" @submit.prevent="login">
       <label class="form-label" for="#email">Email:</label>
-      <input class="form-input" type="email" id="email" required placeholder="Email">
+      <input 
+      v-model="email" 
+      class="form-input" 
+      type="email" 
+      id="email" 
+      required placeholder="Email">
       <label class="form-label" for="#password">Password:</label>
-      <input class="form-input" type="password" id="password" placeholder="Password">
+      <input 
+      v-model="password" 
+      class="form-input" 
+      type="password" 
+      id="password" 
+      placeholder="Password">
+      <p v-if="error" class="error">Has introducido mal el email o la contraseña.</p>
       <input class="form-submit" type="submit" value="Login">
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data: () => ({
+        email: "",
+        password: ""
+    }),
+    //metodos para enviar la petición de login
+    methods: {
+        login() {
+            console.log(this.email);
+            console.log(this.password);
+        }
+    }
+};
+
 </script>
 
 <style lang="scss" scoped>
